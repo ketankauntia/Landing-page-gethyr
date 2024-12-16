da3076ccbf382a1fee8f96d56eec8a8289a9ca24
Ketan Kauntia <ketankauntia26@gmail.com>
2024-12-17 02:13:59 +0530

    required section created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { AboutProgramSection } from "@/components/AboutProgram";
+import { Case } from "@/components/case";
+import { CoordinatorSection } from "@/components/Coordinatior";
+import { CTA } from "@/components/cta";
+import { FAQ } from "@/components/faq";
+import { Footer } from "@/components/footer";
 import { Hero } from "@/components/hero";
+import { ResearchGroupSection } from "@/components/ResearchGroup";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Case />
+      <ResearchGroupSection />
+      <AboutProgramSection />
+      <CoordinatorSection />
+      <FAQ />
+      <CTA />
+      <Footer />
     </div>
   );
 }
